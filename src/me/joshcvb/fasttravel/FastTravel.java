e448318bd8c3f64b4a3df47912a98e3d7fa070a8
Josh <joshcvb@gmail.com>
2012-08-06 15:21:48 -0400

    Initial Push

---
+++ src/me/joshcvb/fasttravel/FastTravel.java
@@ -0,0 +1,29 @@
+package me.joshcvb.fasttravel;
+
+import org.spout.api.Engine;
+import org.spout.api.Spout;
+import org.spout.api.command.CommandRegistrationsFactory;
+import org.spout.api.plugin.CommonPlugin;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+public class FastTravel extends CommonPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+    
+
+}
